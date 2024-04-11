ae82c79ce1d8e33cec43eed172024287b12dcee8
Stjoo0925 <stjoo0925@gmail.com>
2024-04-11 18:09:54 +0900

    first commit

--- chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/ApplicationEx.java
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/ApplicationEx.java
@@ -17,5 +17,35 @@ public class ApplicationEx {
         System.out.println( 1.23 * 10.0 );
         System.out.println( 1.23 / 10 );
         System.out.println( 1.23 + 1.23 );
+
+        //목차 1-3.
+        System.out.println( 123 + 0.5 );
+        System.out.println( 123 - 0.5 );
+        System.out.println( 123 * 0.5 );
+        System.out.println( 123 / 0.5 );
+        System.out.println( 123 % 0.5 );
+
+        //목차 2-1.
+        System.out.println( 'a' + 'b' );
+        System.out.println( 'a' - 'b' );
+        System.out.println( 'a' * 'b' );
+        System.out.println( 'a' / 'b' );
+        System.out.println( 'a' % 'b' );
+
+        //목차 2-2.
+        System.out.println( 'a' + 1 );
+        System.out.println( 'a' - 1 );
+        System.out.println( 'a' * 2 );
+        System.out.println( 'a' / 2 );
+        System.out.println( 'a' % 2 );
+
+        //목차 2-3.
+        System.out.println( 'a' + 1.0 );
+        System.out.println( 'a' - 1.0 );
+        System.out.println( 'a' * 2.0 );
+        System.out.println( 'a' / 2.0 );
+        System.out.println( 'a' % 2.0 );
+
+        
     }
 }

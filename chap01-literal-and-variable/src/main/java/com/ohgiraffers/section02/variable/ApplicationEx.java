b993d77ae883aa42a33e989c3d0766ff023c333e
Stjoo0925 <stjoo0925@gmail.com>
2024-04-12 15:04:08 +0900

    section3

---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section02/variable/ApplicationEx.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section02.variable;
+
+public class ApplicationEx {
+
+    public static void main(String[] args) {
+
+        String name = "주순태";
+        int age = 33;
+        char gender = '남';
+
+        System.out.println( "저의 이름은 " + name + "입니다. 나이는 " + age + "살 이고, 성별은 " + gender + "성 입니다.");
+    }
+}
